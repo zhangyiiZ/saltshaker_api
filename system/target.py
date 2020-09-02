4ad8c4917c051929ff69269fbd28fd1f8cde57e1
ZhangYi <zhangyi2@chinatelecom.cn>
2020-09-02 16:03:50 +0800

    增加监控目标的后端

---
+++ system/target.py
@@ -0,0 +1,163 @@
+# -*- coding:utf-8 -*-
+from flask_restful import Resource, reqparse
+from flask import g
+from common.log import loggers
+from common.audit_log import audit_log
+from common.db import DB
+from common.utility import uuid_prefix
+from common.sso import access_required
+import json
+from system.user import update_user_privilege, update_user_product
+from common.const import role_dict
+from fileserver.rsync_fs import rsync_config
+from common.saltstack_api import SaltAPI
+import gitlab
+
+logger = loggers()
+
+parser = reqparse.RequestParser()
+parser.add_argument("target", type=str, required=True, trim=True)
+parser.add_argument("IP", type=str, required=True, trim=True)
+parser.add_argument("location", type=str, required=True, trim=True)
+parser.add_argument("model", type=str, required=True, trim=True)
+parser.add_argument("type", type=str, required=True, trim=True)
+parser.add_argument("project", type=str, required=True, trim=True)
+parser.add_argument("client", type=str, required=True, trim=True)
+parser.add_argument("pool", type=str, required=True, trim=True)
+
+class Target(Resource):
+    @access_required(role_dict["common_user"])
+    def get(self, target_id):
+        db = DB()
+        status, result = db.select_by_id("product", target_id)
+        db.close_mysql()
+        if status is True:
+            if result:
+                return {"data": result, "status": True, "message": ""}, 200
+            else:
+                return {"status": False, "message": "%s does not exist" % target_id}, 404
+        else:
+            return {"status": False, "message": result}, 500
+
+    @access_required(role_dict["product"])
+    def delete(self, target_id):
+        user = g.user_info["username"]
+        db = DB()
+        status, result = db.delete_by_id("product", target_id)
+        db.close_mysql()
+        if status is not True:
+            logger.error("Delete product error: %s" % result)
+            return {"status": False, "message": result}, 500
+        if result is 0:
+            return {"status": False, "message": "%s does not exist" % target_id}, 404
+        audit_log(user, target_id, target_id, "product", "delete")
+        info = update_user_privilege("product", target_id)
+        if info["status"] is False:
+            return {"status": False, "message": info["message"]}, 500
+        # 更新Rsync配置
+        rsync_config()
+        return {"status": True, "message": ""}, 200
+
+    @access_required(role_dict["product"])
+    def put(self, target_id):
+        user = g.user_info["username"]
+        args = parser.parse_args()
+        args["id"] = target_id
+        product = args
+        db = DB()
+        # 判断是否存在
+        select_status, select_result = db.select_by_id("product", target_id)
+        if select_status is not True:
+            db.close_mysql()
+            logger.error("Modify product error: %s" % select_result)
+            return {"status": False, "message": select_result}, 500
+        if not select_result:
+            db.close_mysql()
+            return {"status": False, "message": "%s does not exist" % target_id}, 404
+        # 判断名字是否重复
+        status, result = db.select("product", "where data -> '$.name'='%s'" % args["target"])
+        if status is True:
+            if result:
+                if target_id != result[0].get("id"):
+                    db.close_mysql()
+                    return {"status": False, "message": "The product name already exists"}, 200
+        status, result = db.update_by_id("product", json.dumps(product, ensure_ascii=False), target_id)
+        db.close_mysql()
+        if status is not True:
+            logger.error("Modify product error: %s" % result)
+            return {"status": False, "message": result}, 500
+        audit_log(user, args["id"], target_id, "product", "edit")
+
+class TargetList(Resource):
+    @access_required(role_dict["common_user"])
+    def get(self):
+        db = DB()
+        user_info = g.user_info
+        role_sql = []
+        if user_info["role"]:
+            for role in user_info["role"]:
+                role_sql.append("data -> '$.id'='%s'" % role)
+            sql = " or ".join(role_sql)
+            role_status, role_result = db.select("role", "where %s" % sql)
+            if role_status and role_result:
+                for role in role_result:
+                    if role["tag"] == 0:
+                        status, result = db.select("product", "")
+                        db.close_mysql()
+                        target_list = []
+                        if status is True:
+                            if result:
+                                target_list = result
+                        else:
+                            return {"status": False, "message": result}, 500
+                        return {"data": target_list, "status": True, "message": ""}, 200
+
+        sql_list = []
+        target_list = []
+        if user_info["product"]:
+            for product in user_info["product"]:
+                sql_list.append("data -> '$.id'='%s'" % product)
+            sql = " or ".join(sql_list)
+            status, result = db.select("product", "where %s" % sql)
+            db.close_mysql()
+            if status is True:
+                if result:
+                    product_list = result
+                    return {"data": product_list, "status": True, "message": ""}, 200
+                else:
+                    return {"status": False, "message": "Group does not exist"}, 404
+            else:
+                return {"status": False, "message": result}, 500
+        return {"data": target_list, "status": True, "message": ""}, 200
+
+    @access_required(role_dict["product"])
+    def post(self):
+        args = parser.parse_args()
+        args["id"] = uuid_prefix("p")
+        user = g.user_info["username"]
+        user_id = g.user_info["id"]
+        product = args
+        db = DB()
+        status, result = db.select("product", "where data -> '$.name'='%s'" % args["target"])
+        if status is True:
+            if len(result) == 0:
+                # 给用户添加产品线
+                info = update_user_product(user_id, args["id"])
+                if info["status"] is False:
+                    return {"status": False, "message": info["message"]}, 500
+                insert_status, insert_result = db.insert("product", json.dumps(product, ensure_ascii=False))
+                db.close_mysql()
+                if insert_status is not True:
+                    logger.error("Add product error: %s" % insert_result)
+                    return {"status": False, "message": insert_result}, 500
+                audit_log(user, args["id"], "", "product", "add")
+            else:
+                db.close_mysql()
+                return {"status": False, "message": "The product name already exists"}, 200
+        else:
+            db.close_mysql()
+            logger.error("Select product name error: %s" % result)
+            return {"status": False, "message": result}, 500
+
+
+
