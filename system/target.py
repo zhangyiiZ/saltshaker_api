8d2e136654fbf90f0a205efc8edeb38eca384a30
ZhangYi <zhangyi2@chinatelecom.cn>
2020-09-04 10:26:38 +0800

    测试file的api

--- system/target.py
+++ system/target.py
@@ -169,9 +169,9 @@ class UploadTarget(Resource):
     def post(self):
         file = request.files['file']
         logger.info("firename:"+file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        file.save(os.path.join('/tmp', file.filename))
         content = file.read()
-        logger.info("path:"+os.path.join('/tmp', file.filename))
+        logger.info("path:")
         return {"status": True, "message": ""}, 200
         try:
             content_decode = content.decode()
