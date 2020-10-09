312d29e5560005696b5f3f78f767f6fd58f3aaf0
ZhangYi <zhangyi2@chinatelecom.cn>
2020-10-09 15:15:16 +0800

    稍作修改

--- resources/git_config.py
+++ resources/git_config.py
@@ -154,7 +154,7 @@ class UploadConfig(Resource):
     def post(self):
         args = parser.parse_args()
         project, product_name = get_gitlab_project(args["project_id"], args["product_id"])
-        logger.info('project:'+project)
+        logger.info('project:'+str(product_name))
         file = request.files['file']
         if args["path"]:
             file_path = args["path"] + "/" + file.filename
